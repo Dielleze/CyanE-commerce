96ae16bc7db154a2a5230d0fa86e7e664eb036fe
Diellezah <djellza@centralsoft.io>
2023-03-13 00:45:56 +0100

    feat: hiding paths when user logs/registers

--- e-commerce-server/server.js
+++ e-commerce-server/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
-
+const cors = require('cors');
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:8080/',
+}))
 
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
 const port = 3000;
+
+app.get('*', (req,res)=>{
+    res.json({success:true});
+})
 
 app.listen(port, ()=>{
     console.log(`The server is listening in port ${port}`)
