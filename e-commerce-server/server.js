913afe5198155586967be41eb99d0f1444203976
Diellezah <djellza@centralsoft.io>
2023-03-13 21:48:18 +0100

    mongo database connect

--- e-commerce-server/server.js
+++ e-commerce-server/server.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const cors = require('cors');
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:8080/',
-}))
+const mongoose = require('mongoose');
 
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json())
-const port = 3000;
-
-app.get('*', (req,res)=>{
-    res.json({success:true});
-})
-
-app.listen(port, ()=>{
-    console.log(`The server is listening in port ${port}`)
+mongoose.connect("mongodb://localhost:27017/CyanDB").then(()=>{
+    console.log("Conected to db");
+    app.use(cors({
+        origin: 'http://localhost:8080/',
+    }))
+    
+    app.use(express.urlencoded({ extended: true}))
+    app.use(express.json())
+    const port = 3000;
+    
+    app.get('*', (req,res)=>{
+        res.json({success:true});
+    })
+    
+    app.listen(port, ()=>{
+        console.log(`The server is listening in port ${port}`)
+    })
 })
