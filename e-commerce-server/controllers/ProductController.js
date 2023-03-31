c94118f9be2dfe4b561e6dd32e00829d63a72800
Djellza Helshani <djellzahelshani@gmail.com>
2023-03-31 14:22:42 +0200

    coordinating files

---
+++ e-commerce-server/controllers/ProductController.js
@@ -0,0 +1,68 @@
+const { createProduct } = require('e-commerce-server/validators/create');
+const { updateProduct} = require('e-commerce-server/validators/update');
+
+const productsModel = require ('../models/productsModel')
+import { StatusCodes, ReasonPhrases} from 'http-status-codes'
+
+const controller = {
+    list: async(req,res)=>{
+        const list = await productsModel.find();
+        return res.json(list);
+    },
+    find:async(req,res)=>{
+        try {
+            const products = await productsModel.findOne({ _id: req.params.id});
+            if(!products) throw Error("PRoduct not found");
+            return res.json(products);
+        } catch (
+            error
+        ) {
+            res.status(404).json({ error: error.message})        
+        }
+    },
+    post: async (req,res)=>{
+        const validateResult = createProduct.validate(req.body)
+        if(validateResult.error){
+            return res.status(StatusCodes.UNAUTHORIZED)
+            .json({ message: ReasonPhrases.UNAUTHORIZED,
+                     error: validateResult.error.message})
+        }
+        const newProduct = new productsModel(req.body);
+        
+        try{
+            await newProduct.save()
+            return res.json(newProduct)
+        } catch(err){
+            return res.json(StatusCodes.UNAUTHORIZED)
+            .json({ message: ReasonPhrases.UNAUTHORIZED,
+                    error: validateResult.error.message})
+        }
+    },
+    post: async(req, res)=>{
+        const validateResult = updateProduct.validate(req.body)
+    
+        if(validateResult.error){
+            return res.status(StatusCodes.UNAUTHORIZED).json({ message: ReasonPhrases.UNAUTHORIZED, error: validateResult.error.message})
+        }
+    
+        try{
+            await productsModel.updateOne({_id: req.params.id}, req.body);
+            const updatedPRoduct = await productsModel.find({_id: req.params.id});
+            return res.json(updatedPRoduct);
+        }catch(err){
+            return res.json(StatusCodes.UNAUTHORIZED)
+            .json({ message: ReasonPhrases.UNAUTHORIZED,
+                    error: validateResult.error.message})
+        }
+    },
+    delete:  async(req,res)=>{
+        const productid = req.params.id;
+        try {
+            await productsModel.deleteOne({ _id: productid})
+        } catch (error) {
+            res.status(StatusCodes.NOT_FOUND).json({ message: ReasonPhrases.NOT_FOUND });
+        }
+    }
+}
+
+export default controller;
--- e-commerce-server/validators/update.js
+++ e-commerce-server/validators/update.js
@@ -1,8 +1,8 @@
 const Joi = require('joi')
 
-const updateProduct = Joi.object({
+const createProduct = Joi.object({
     title: Joi.string().required().min(5).max(250),
     description: Joi.string().allow(null)
 })
 
-export default updateProduct;
+export default createProduct;
